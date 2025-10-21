5f00a67ee8709eda9308b8aa98f4ba3fc10b4996
mfatoni525-del <mfatoni525@gmail.com>
2025-10-21 13:24:17 +0700

    Create script.js

---
+++ script.js/script.js
@@ -0,0 +1,7 @@
+//1.PILIH tombol berdasarkan ID-nya
+const tomnbolSapa =Document.getElementByID{sapaButton};
+// 2. Tambahkan event listener untuk aksi click
+tombolSapa.addEventListener[click function
+  // 3. Jalankan kode ini ketika tombol di-klik
+alert [Halo!Terima kasih sudah berkunjung!];
+});
